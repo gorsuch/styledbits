c6ddc1ecdd17adece4518f93a019cd682c825159
Michael Gorsuch <michael.gorsuch@gmail.com>
2010-11-12 20:21:51 -0500

    first checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Styledbits::Application.config.secret_token = 'a67640be56d5d6625cca1469a69f722664c88386740381291e4cfdaa4c909ca9a887d9d487cdaa4af8fea4e489a856f04c1d77e69ee4a1b3426a0d10aec082aa'
